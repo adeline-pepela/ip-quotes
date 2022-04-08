0f038cb919958b58c5d3f1c01b691f76c2a236d6
adeline-pepela <adelinemakokha@gmail.com>
2022-04-08 14:58:52 +0300

    adding quotes on our quotes component.ts

--- quote/src/app/quote/quote.component.ts
+++ quote/src/app/quote/quote.component.ts
@@ -9,6 +9,10 @@ export class QuoteComponent implements OnInit {
 quotes:Quote[]=[
   {id:1,name:"Martin Fowler" , description:"“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.”"},
   {id:2,name:"John Johnson " , description: "“First, solve the problem. Then, write the code.” "},
+  {id:3,name:"Coco Chanel" , description: "In order to be irreplaceable, one must always be different"},
+  {id:4,name:"Chris Heilmann" , description: "“Java is to JavaScript what car is to Carpet.” "},
+  {id:5,name:"Dan Salomon" , description:"“Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code.”"},
+  {id:6,name:"Kent Beck", description:"“Make it work, make it right, make it fast.” "},
 ]
   constructor() { }
 
