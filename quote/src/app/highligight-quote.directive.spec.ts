894e3759d8ca05ff7ec791911b2b784f6b8673b4
adeline-pepela <adelinemakokha@gmail.com>
2022-04-13 09:03:11 +0300

    Auto-generated commit [ci skip]

--- quote/src/app/highligight-quote.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighligightQuoteDirective } from './highligight-quote.directive';
-
-describe('HighligightQuoteDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighligightQuoteDirective();
-    expect(directive).toBeTruthy();
-  });
-});
